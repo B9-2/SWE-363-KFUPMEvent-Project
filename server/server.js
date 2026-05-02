b353eaf97e362bdad2a0f41f2b80d03600e72cd9
B9-2 <s202260760@kfupm.edu.sa>
2026-05-02 18:54:27 +0300

    Prepare backend for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -11,9 +11,9 @@ import errorHandler from "./middleware/errorHandler.js";
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -37,8 +37,4 @@ app.use((req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
